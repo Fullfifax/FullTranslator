d9b672a47979df807096598130826ec32b5e6f84
Fullfifax <fullfifax1245@gmail.com>
2021-05-02 13:31:12 +0200

    App back-end (googletrans...)

---
+++ traducteur.py
@@ -0,0 +1,21 @@
+from googletrans import Translator, LANGUAGES
+
+class Traduction():
+	def __init__(self):
+		self.languageList = list(LANGUAGES.values())
+	def translation(self, txt="Type here", src = LANGUAGES.values(), dest = LANGUAGES.values()):
+		self.traduire  = Translator()
+		self.txt = txt
+		self.src = src
+		self.dest = dest
+		try:
+			self.translated = self.traduire.translate(self.txt, src = self.src, dest = self.dest)
+		except:
+			self.translated = self.traduire.translate(self.txt)
+		self.ttext = self.translated.text
+		return self.ttext
+
+
+
+
+
